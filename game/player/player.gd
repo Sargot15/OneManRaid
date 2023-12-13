3e31af0be895ee6759bd8c08f52a0d13c38c70a3
cfernandezvegue <carlos.fernandezvegue@gmail.com>
2023-12-13 22:42:43 +0100

    Added player movement

---
+++ game/player/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var speed = 300
+
+var direction = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	move_player()
+	
+func move_player():
+	direction.x = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
+	direction.y = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))
+	
+	velocity = direction.normalized() * speed
+	
+	move_and_slide()
