82406f77a3b52a0259768fa02a58bf378cb020a6
cfernandezvegue <carlos.fernandezvegue@gmail.com>
2024-02-06 23:52:17 +0100

    First approach to change heroes

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @export var speed = 300
+@export var heroes : Array[Node2D]
 
 var direction = Vector2.ZERO
+var actual_hero : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +14,15 @@ func _ready():
 func _physics_process(delta):
 	move_player()
 	
+	if (Input.is_key_pressed(KEY_1)):
+		change_hero(0)
+	if (Input.is_key_pressed(KEY_2)):
+		change_hero(1)
+	if (Input.is_key_pressed(KEY_3)):
+		change_hero(2)
+	if (Input.is_key_pressed(KEY_4)):
+		change_hero(3)
+	
 func move_player():
 	direction.x = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
 	direction.y = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))
@@ -19,3 +30,7 @@ func move_player():
 	velocity = direction.normalized() * speed
 	
 	move_and_slide()
+	
+func change_hero(index: int):
+	actual_hero = heroes[index]
+	$Sprite2D.texture = actual_hero.get_node("Sprite2D").texture
