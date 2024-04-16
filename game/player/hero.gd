818ac1e56613f8e48d0e6f5cf7fb5ee8ca3939ac
Sargot15 <carlos.fernandezvegue@gmail.com>
2024-04-16 23:13:04 +0200

    Limit hero shoot latency

--- game/player/hero.gd
+++ game/player/hero.gd
@@ -6,13 +6,21 @@ signal health_updated(hero_type : Globals.COLOR_TYPE ,health : int,max_health : 
 
 @export var hero_type : Globals.COLOR_TYPE
 
+var time_to_next_attack : float = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health_updated.emit(hero_type, $HeroStats.health, $HeroStats.max_health)
+	time_to_next_attack = 0
 
-		
+func _process(delta):
+	if (time_to_next_attack > 0):
+		time_to_next_attack -= delta
+
 func shoot():
-	weapon.shoot()
+	if (time_to_next_attack <= 0):
+		weapon.shoot()
+		time_to_next_attack = 1 / $HeroStats.attack_speed
 	
 func take_damage(damage : float, color_type : Globals.COLOR_TYPE):
 	# Bullet does not do damage if color type are the same
