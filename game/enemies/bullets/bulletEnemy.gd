94a32f7cf233383683ea8ea747caf30013d4877c
Sargot15 <carlos.fernandezvegue@gmail.com>
2024-05-15 23:13:02 +0200

    Add gray bullet

--- game/enemies/bullets/bulletEnemy.gd
+++ game/enemies/bullets/bulletEnemy.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 @onready var kill_timer = $KillTimer
+@onready var hit_again_timer = $HitAgainTimer
 
 @export var speed : float = 400
 @export var damage : float
 @export var is_static : bool = false
+@export var destroy_on_collistion : bool = true
 @export var type : Globals.COLOR_TYPE
+
+var hit_recently : bool = false
 
 func _ready():
 	Globals.debug_total_enemy_bullets += 1
@@ -22,11 +26,19 @@ func _on_kill_timer_timeout():
 
 
 func _on_body_entered(body):
-	if(body.has_method("take_damage")):
+	if(body.has_method("take_damage") && !hit_recently):
 		body.take_damage(damage, type)
+		hit_recently = true
+		hit_again_timer.start()
 	
-	Globals.debug_total_enemy_bullets -= 1
-	queue_free()
+	if (destroy_on_collistion):
+		Globals.debug_total_enemy_bullets -= 1
+		queue_free()
+
+
+func _on_hit_again_timer_timeout():
+	hit_recently = false
+
 
 func set_time_alive(time_alive : float):
 	kill_timer.stop()
--- game/globals/globals.gd
+++ game/globals/globals.gd
@@ -1,5 +1,5 @@
 extends Node
 
-enum COLOR_TYPE {YELLOW, BLUE, BROWN, GREEN}
+enum COLOR_TYPE {YELLOW, BLUE, BROWN, GREEN, GRAY}
 
 var debug_total_enemy_bullets : int = 0
