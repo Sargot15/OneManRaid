bcfe3d1e95245b2c457967d5ae65bb88dd9c70bd
Sargot15 <carlos.fernandezvegue@gmail.com>
2025-04-10 22:27:51 +0200

    Center floating text instanciated

--- game/globals/floatingTextManager.gd
+++ game/globals/floatingTextManager.gd
@@ -17,7 +17,8 @@ func show_heal_text(amount : int, world_position : Vector2):
 
 func show_text(amount : int, world_position : Vector2, color : Color):
 	var text_instance = FloatingTextScene.instantiate()
-	text_instance.global_position = world_position + Vector2(randf_range(-20, 20), randf_range(-20, 20))
 	text_instance.text = " " + str(amount) + " " # Added spaces so the outline of the text shows correctly
+	text_instance.global_position = world_position + Vector2(randf_range(-20, 20) - (text_instance.size.x / 2), randf_range(-20, 0))
 	text_instance.text_color = color
 	get_tree().current_scene.add_child(text_instance)
+
