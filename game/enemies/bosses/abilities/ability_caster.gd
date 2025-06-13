fac13d5e9562b67e4b1fc7cf4bbe76f9c4380919
Sargot15 <carlos.fernandezvegue@gmail.com>
2025-06-13 20:59:08 +0200

    #0080 Some improvements to the timing on casting abilities for the boss

--- game/enemies/bosses/abilities/ability_caster.gd
+++ game/enemies/bosses/abilities/ability_caster.gd
@@ -1,9 +1,14 @@
 class_name AbilityCaster extends Node
 
 @export var boss : Boss
+## At the beginning of the fight and just after casting an ability, this is the chance to cast another ability
+@export_range(0, 1) var initial_chance_to_cast : float = 0
+## Every time there is a try to cast an ability and it failed, the chance to cast an ability will increase this amount for the next try
+@export_range(0, 1) var increased_chance_to_cast_after_try : float
 
 var abilities : Array[Node]
 var can_cast : bool = false
+var actual_chance_to_cast : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +22,7 @@ func start() -> void:
 	if not can_cast:
 		can_cast = true
 		$TryCastAbilityTimer.start()
+		actual_chance_to_cast = initial_chance_to_cast
 	
 func stop() -> void:
 	if can_cast:
@@ -51,6 +57,12 @@ func _on_try_cast_ability_timer_timeout():
 	if not can_cast:
 		return
 
+	# Try to cast an ability
+	actual_chance_to_cast += increased_chance_to_cast_after_try
+	if randf() > actual_chance_to_cast:
+		return
+	print ("Casting after a " + str(actual_chance_to_cast) + " chance")
+
 	var total_weight : float = 0
 	
 	for ability in abilities:
@@ -65,6 +77,7 @@ func _on_try_cast_ability_timer_timeout():
 			accumulated_weight += ability.weight
 			if pick < accumulated_weight:
 				ability.cast()
+				actual_chance_to_cast = initial_chance_to_cast
 				if ability.is_unique_cast:
 					can_cast = false
 					ability.connect("finished_cast", _on_ability_finished_cast)
