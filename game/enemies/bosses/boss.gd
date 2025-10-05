428063b23ef6be3531477182797aab9bd6ee0792
Sargot15 <carlos.fernandezvegue@gmail.com>
2025-10-05 22:56:49 +0200

    #0086 Added fight timer to boss

--- game/enemies/bosses/boss.gd
+++ game/enemies/bosses/boss.gd
@@ -4,7 +4,8 @@ class_name Boss extends Node2D
 @export var max_health : float
 
 @onready var phase_manager = $PhaseManager
-@onready var health_bar : ProgressBar = $CanvasLayer/HealthBar
+@onready var health_bar : ProgressBar = $CanvasLayer/FightBossInfoContainer/HealthBar
+@onready var fight_time_label : Label = $CanvasLayer/FightBossInfoContainer/FightTimeLabel
 
 # Actual stats
 var health : float
@@ -12,16 +13,22 @@ var actual_phase : int
 
 var updating_health : bool = false
 var fight_started : bool = false
+var fight_time : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = max_health
 	_update_health_bar()
+	
+	fight_time = 0.0
+	_update_fight_timer_label()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if fight_started:
+		fight_time += delta
+		_update_fight_timer_label()
 
 func take_damage(damage : float) -> void:
 	# If the boss is hit and the fight was not started, then it starts
@@ -58,6 +65,12 @@ func _start_fight() -> void:
 func _update_health_bar() -> void:
 	health_bar.max_value = max_health
 	health_bar.value = health
+	
+
+func _update_fight_timer_label():
+	var minutes = int(fight_time) / 60
+	var seconds = int(fight_time) % 60
+	fight_time_label.text = "%02d:%02d" % [minutes, seconds]
 
 
 func _on_player_detection_body_entered(body):
