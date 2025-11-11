9715ef811ad3659b41200b164cb75a006be0bd5a
Sargot15 <carlos.fernandezvegue@gmail.com>
2025-11-11 20:13:22 +0100

    #0100 Fix movement by mouse when clicking in UI

--- game/player/player.gd
+++ game/player/player.gd
@@ -3,11 +3,13 @@ class_name Player extends CharacterBody2D
 signal hero_health_update(hero_type : Globals.COLOR_TYPE, health : int, max_health : int)
 signal stats_updated(hero_type : Globals.COLOR_TYPE ,def : float, att : float, spd : float, att_range : float, att_speed : float)
 
-@export var heroes : Array[Node2D]
+@export var heroes : Array[Hero]
 
 var direction = Vector2.ZERO
-var actual_hero : Node2D
+var actual_hero : Hero
 var actual_hero_index : int = 0
+
+var moving_by_mouse : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,41 +23,44 @@ func _physics_process(delta):
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		actual_hero.shoot()
-	
-	# Var to change hero in case the key is pressed
-	var new_hero_index = -1
-	
-	if Input.is_key_pressed(KEY_1):
-		new_hero_index = 0
-	if Input.is_key_pressed(KEY_2):
-		new_hero_index = 1
-	if Input.is_key_pressed(KEY_3):
-		new_hero_index = 2
-	if Input.is_key_pressed(KEY_4):
-		new_hero_index = 3
 		
-	if (new_hero_index != -1 && new_hero_index != actual_hero_index):
-		_change_hero(new_hero_index)
+func _unhandled_input(event) -> void:
+	# Detect movement by mouse
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			moving_by_mouse = true
+		else:
+			moving_by_mouse = false
+		
+	# Detect change hero	
+	if event.is_action_pressed("ui_change_hero_1"):
+		_change_hero(0)
+	if event.is_action_pressed("ui_change_hero_2"):
+		_change_hero(1)
+	if event.is_action_pressed("ui_change_hero_3"):
+		_change_hero(2)
+	if event.is_action_pressed("ui_change_hero_4"):
+		_change_hero(3)
 	
 func _move_player(delta) -> void:
 	var speed = actual_hero.get_speed()
-	# Detect movement by keys
-	direction.x = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
-	direction.y = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))
 	
-	# Detect movement by mouse (mouse is priorized)
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		direction.x = int(get_global_mouse_position().x - global_position.x)
-		direction.y = int(get_global_mouse_position().y - global_position.y)
+	if moving_by_mouse:
+		direction = get_global_mouse_position() - global_position
 		
 		# If mouse is very close to the mouse the speed is slower so the player is not "dancing" around the mouse
 		var distance_to_mouse = global_position.distance_to(get_global_mouse_position())
 		if distance_to_mouse < speed * delta:
 			speed = distance_to_mouse / delta
-	
+	else:
+		# Detect movement by keys
+		direction.x = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
+		direction.y = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))
+		
 	velocity = velocity.lerp(direction.normalized() * speed, 1.0 - exp(-15 * delta))
 	
 	move_and_slide()
+
 	
 # Tries to change the hero, returns true it is possible, false if it is not
 func _change_hero(new_hero_index: int) -> bool:
